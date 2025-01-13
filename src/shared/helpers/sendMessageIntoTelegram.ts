da942aa87fd82b59562b68eec553269456e2eaff
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-01-13 10:46:47 +0200

    feat: added some changes

--- src/shared/helpers/sendMessageIntoTelegram.ts
+++ src/shared/helpers/sendMessageIntoTelegram.ts
@@ -2,8 +2,8 @@ export const sendMessage = async (message: string): Promise<void> => {
   try {
     console.log(message);
     // const URL = `${URI_API}${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text${message}`;
-    // const URL = `https://api.telegram.org/bot7077738581:AAEqoAWJvox6ouc6foEcZNLTNWO6N8MSaNw/sendMessage?chat_id=-1001682516809&text=site production ${message}`;
-    // await fetch(URL);
+    const URL = `https://api.telegram.org/bot7077738581:AAEqoAWJvox6ouc6foEcZNLTNWO6N8MSaNw/sendMessage?chat_id=-1001682516809&text=site production ${message}`;
+    await fetch(URL);
   } catch (e) {
     console.log(e);
   }
